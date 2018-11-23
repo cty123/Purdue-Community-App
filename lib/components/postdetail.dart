c55b48bca5d6ac466724612bb2583e75eef7b306
cty123 <chen2216@purdue.edu>
2018-11-23 17:26:09 -0500

    Add rich text edit and display for post detail page

---
+++ lib/components/postdetail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/models/post.dart';
+
+class PostDetailItem extends StatelessWidget {
+  final Post _post;
+
+  PostDetailItem(this._post);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card();
+  }
+}
--- lib/screens/postdetail.dart
+++ lib/screens/postdetail.dart
@@ -1,19 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/models/post.dart';
+import 'package:zefyr/zefyr.dart';
+import 'package:hello_world/components/postdetail.dart';
+import 'dart:convert';
 
-class PostDetail extends StatelessWidget {
+class PostDetail extends StatefulWidget {
   final Post post;
 
   PostDetail(this.post);
 
+  _PostDetail createState() => _PostDetail();
+}
+
+class _PostDetail extends State<PostDetail> {
+  ZefyrController _controller;
+  FocusNode _focusNode;
+  NotusDocument _document;
+
+  @override
+  void initState() {
+    super.initState();
+    // Create an empty document or load existing if you have one.
+    // Here we create an empty document:
+    _document = new NotusDocument();
+    _controller = new ZefyrController(_document);
+    _focusNode = new FocusNode();
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: new AppBar(
-        title: new Text('${post.title}'),
+        title: Row(children: <Widget>[
+          new Text('${widget.post.title}'),
+          // new IconButton(
+          //   icon: Icon(Icons.touch_app),
+          //   onPressed: (){
+          //     var data = json.decode(json.encode(_controller.document.toJson()));
+          //     NotusDocument nd = new NotusDocument.fromJson(data);
+          //     print(nd.toString());
+          //     setState(() {
+          //       _controller = new ZefyrController(nd);           
+          //     });
+          //   },
+          // )
+        ],),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.control_point), onPressed: null,),
+          IconButton(icon: Icon(Icons.edit), onPressed: null,),
+          IconButton(icon: Icon(Icons.comment), onPressed: null,),
+        ]
       ),
 
-      body: Text('${post.content}'),
+      body: ZefyrScaffold(
+        child: new Card(
+          child: ZefyrEditor(
+            enabled: true,
+            controller: _controller,
+            focusNode: _focusNode,
+          ),
+        )
+      )
     );
   }
 }
